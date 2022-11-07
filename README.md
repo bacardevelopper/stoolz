5381095996c7e8090004db2cbaf427b10399b5e0
niwradCodera <63006659+bacardevelopper@users.noreply.github.com>
2022-11-07 20:49:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# stoolz
+
+## Explication
+
+Un application de gestion du materiel des conseillers numériques, outils non officiel.
---
+++ android/app/src/main/kotlin/com/example/stoolz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stoolz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
